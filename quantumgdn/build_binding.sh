f13aa83fe638018f1407dd7f329ae5374f0fc250
DaedricSpartan99 <leleraffa97@hotmail.it>
2020-04-04 20:53:50 +0200

    Windows configuration

--- quantumgdn/build_binding.sh
+++ quantumgdn/build_binding.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-scons platform=linux generate_bindings=yes -j2 use_custom_api_file=yes custom_api_file=../api.json bits=64
+scons platform=x11 generate_bindings=yes -j2 use_custom_api_file=yes custom_api_file=../api.json bits=64
---
+++ quantumgdn/build_win.bat
@@ -0,0 +1,1 @@
+scons platform=windows generate_bindings=yes -j4 use_custom_api_file=yes custom_api_file=../api.json bits=64
