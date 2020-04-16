8c4150975451337714b8d6a437a0b00de8e2b11e
Saara <34299252+astonedf@users.noreply.github.com>
2020-04-16 19:35:58 +0200

    OptionsScreen added

--- WarmUp/scripts/TitleScreen.gd
+++ WarmUp/scripts/TitleScreen.gd
@@ -1,20 +1,29 @@
 extends Control
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
-#	pass
+
+
 
 #Pressing NewGameButton takes to main scene
 func _on_NewGameButton_pressed():
 	get_tree().change_scene("res://scenes/Main.tscn")
+
+
+func _on_NewGameButton_mouse_entered():
+	$Menu/CenterRow/Buttons/NewGameButton/Label.add_color_override("font_color", Color(0.9,0.1,0.9,0.5))
+
+
+func _on_NewGameButton_mouse_exited():
+	$Menu/CenterRow/Buttons/NewGameButton/Label.add_color_override("font_color", Color(1,1,1,1))
+
+
+func _on_OptionsButton_pressed():
+	get_tree().change_scene("res://scenes/Menu/OptionSreen.tscn")
+
