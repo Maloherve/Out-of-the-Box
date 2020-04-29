5a455eca454ae74b669a058db2facfae01c18bb4
Saara <astonedf@users.noreply.github.com>
2020-04-29 21:17:03 +0200

    music continuing over different scenes

--- WarmUp/scripts/Menu/TitleScreen.gd
+++ WarmUp/scripts/Menu/TitleScreen.gd
@@ -3,7 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	if (Music.get_node("GameSoundtrack").playing == false):
+		Music.get_node("GameSoundtrack").play()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
