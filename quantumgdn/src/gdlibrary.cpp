1b7c581732f46594cfbcff84b735fcbf5eee04c3
DaedricSpartan99 <leleraffa97@hotmail.it>
2020-05-06 00:31:06 +0200

    Uniform potential

--- quantumgdn/src/gdlibrary.cpp
+++ quantumgdn/src/gdlibrary.cpp
@@ -1,15 +1,12 @@
 #include <Godot.hpp>
 #include <Node.hpp>
 
-//#include "potential.hpp"
-//#include "uniform_potential.hpp"
-//#include "composed_potential.hpp"
 #include "potential_field.hpp"
 #include "curve_field.hpp"
+#include "uniform_field.hpp"
 #include "curve_potential.hpp"
 #include "grid_wave.hpp"
 #include "qgridsystem1D.hpp"
-//#include "qgridsystem2D.hpp"
 
 
 extern "C" void GDN_EXPORT godot_gdnative_init(godot_gdnative_init_options *o)
@@ -25,20 +22,11 @@ extern "C" void GDN_EXPORT godot_gdnative_terminate(godot_gdnative_terminate_opt
 extern "C" void GDN_EXPORT godot_nativescript_init(void *handle)
 {
 	godot::Godot::nativescript_init(handle);
-    
-    // potentials 1D and super-classes
-	//register_class<curve_potential>();
-
-	//register_class<grid_composed_potential1D>();
-
-	//register_class<grid_uniform_potential1D>();
-
-	//register_class<grid_potential2D>();
-	//register_class<grid_uniform_potential2D>();
 
     // potential fields
 	godot::register_class<godot::potential_field>();
 	godot::register_class<godot::curve_field>();
+	godot::register_class<godot::uniform_field>();
 
     // grid wave
 	godot::register_class<godot::grid_wave>();
@@ -52,9 +40,6 @@ extern "C" void GDN_EXPORT godot_nativescript_init(void *handle)
 	//register_class<wave_init2D>();
 	//register_class<gauss_init2D>();
 
-    // layers
-    //register_class<qsimbox>();
-    
     // systems
     godot::register_class<godot::gdqsystem>();
 	godot::register_class<godot::qgridsystem1D>();
---
+++ quantumgdn/src/uniform_field.cpp
@@ -0,0 +1,35 @@
+#include "uniform_field.hpp"
+#include <TileMap.hpp>
+
+#include "debug.hpp"
+
+using namespace godot;
+
+uniform_field::uniform_field() : value(0.0), tilemap(nullptr) {
+}
+
+double uniform_field::at(const Vector2& r) const {
+    return ((tilemap != nullptr) &&
+             tilemap->get_cellv(tilemap->world_to_map(r)) != TileMap::INVALID_CELL) 
+        ? value : 0.0;
+}
+
+double uniform_field::_at(Vector2 r) const {
+    return at(r);
+}
+
+void uniform_field::_ready() {
+    tilemap = Object::cast_to<TileMap>(get_parent());
+
+    if (tilemap == nullptr) {
+        // TODO, send godot warning
+    }
+}
+
+void uniform_field::_init() {
+}
+
+void uniform_field::_register_methods() {
+    register_method("at", &uniform_field::_at);
+    register_property<uniform_field, double>("value", &uniform_field::value, 0.0);
+}
