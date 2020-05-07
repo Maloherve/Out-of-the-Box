4b3b29c9b15f63a539b4a6091260d8fbd6a87d11
Malo Herve <malo.herve@epfl.ch>
2020-05-07 18:28:24 +0200

    Choose flicker properties in trigger

--- WarmUp/scenes/Levels/Trigger_Flicker.gd
+++ WarmUp/scenes/Levels/Trigger_Flicker.gd
@@ -3,15 +3,20 @@ extends Area2D
 export var light_path : NodePath;	# Light to make flicker
 export var unique : bool = true;	# Only be ably to set the flicker once
 
+export var flicker_delay : float = .07;
+export var iterations : int = 4;
 
+var LightNode : Sprite;
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	connect('body_entered', self, '_on_Area2D_body_entered')
-
+	LightNode = get_node(light_path)
 
 func _on_Area2D_body_entered(body):
-	get_node(light_path).flicker = true;
+	LightNode.flicker = true;
+	LightNode.flicker_delay = flicker_delay;
+	LightNode.iterations = iterations;
 	if unique:
 		queue_free();
