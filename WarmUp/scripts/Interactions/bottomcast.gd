98683a6b78e65462363143f849866c2d529ac433
DaedricSpartan99 <leleraffa97@hotmail.it>
2020-05-18 00:58:51 +0200

    Platform well implemented

--- WarmUp/scripts/Interactions/bottomcast.gd
+++ WarmUp/scripts/Interactions/bottomcast.gd
@@ -10,11 +10,16 @@ func _input(event):
 		if !disabled:
 			get_parent().velocity.y += 80;
 		disabled = true;
-		print("Phantom")
 	elif event.is_action_released(disabler):
 		get_parent().set_collision_mask_bit(collision_bit, true);
 		disabled = false;
-		print("Pirla")
+	
+	if event.is_action_pressed("ui_up"):
+		get_parent().set_collision_mask_bit(collision_bit, false);
+		disabled = true;
+	elif event.is_action_released("ui_up"):
+		get_parent().set_collision_mask_bit(collision_bit, true);
+		disabled = false;
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
