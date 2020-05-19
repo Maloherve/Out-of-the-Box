0b0ffcd10512aad16abed01d975f95013a7bcfb2
Delphine Martres <dmartres@gmail.com>
2020-05-19 16:55:02 +0200

    Scene changer

--- WarmUp/scripts/Intro/Intro.gd
+++ WarmUp/scripts/Intro/Intro.gd
@@ -164,3 +164,5 @@ func _on_Timer_timeout():
 func _on_Scientists_animation_finished():
 	if ($World/Enemies/Scientists.animation == 'leaving'):
 		$World/NPC/DialogueBox.visible = false
+		SceneChanger.change_scene("res://scenes/Levels/Introduction.tscn")
+#		get_tree().change_scene("res://scenes/Levels/Introduction.tscn")
