7bcdde26008d4d7638e3d7ea4ff758acb67a9187
DaedricSpartan99 <leleraffa97@hotmail.it>
2020-05-22 23:23:14 +0200

    Flamethrowers motherfucker

--- WarmUp/scripts/Objects/flamethrower.gd
+++ WarmUp/scripts/Objects/flamethrower.gd
@@ -1,6 +1,7 @@
 extends Particles2D
 
 export (float) var switch_time = 4.0;
+export (float) var shift = 0.0;
 onready var player : KinematicBody2D = get_tree().get_current_scene().get_node("Player");
 
 func enable():
@@ -27,5 +28,7 @@ func _on_timer_timeout():
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	disable();
+	if shift != 0:
+		yield(get_tree().create_timer(shift), "timeout");
 	$clock.connect("timeout", self, "_on_timer_timeout");
 	$clock.start(switch_time);
