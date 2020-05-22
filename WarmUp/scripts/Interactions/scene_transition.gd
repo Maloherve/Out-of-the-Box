8c01b73ea543984afbe46cda5a15d4785f3a6e59
DaedricSpartan99 <leleraffa97@hotmail.it>
2020-05-22 11:37:22 +0200

    Interesting link

--- WarmUp/scripts/Interactions/scene_transition.gd
+++ WarmUp/scripts/Interactions/scene_transition.gd
@@ -1,15 +1,20 @@
+tool
 extends Area2D
 
-export (String, "intro", "introduction", "walls", "floating_platforms", "sensors", "oldlab") var target_level; 
+export (String, "intro", "introduction", "walls", "floating_platforms", "sensors", "oldlab") var target_level;
+export (int) var checkpoint = -1; 
+export (RectangleShape2D) var area = null;
 
 func _on_scene_body_entered(body):
 	if body.name == "Player":
 		#laisse le temps au son d'etre joué avant de changer de scene 
 		$Win.play()
 		yield(get_tree().create_timer(0.25), "timeout")
-		SceneChanger.change_scene(target_level);
+		SceneChanger.change_scene(target_level, checkpoint);
 
 func _init():
 	connect("body_entered", self, "_on_scene_body_entered");
 	
-
+func _ready():
+	assert(area != null)
+	$collider.shape = area;
